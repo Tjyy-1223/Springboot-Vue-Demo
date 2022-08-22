ffa6ce7c0654284fed68ac086185d84ec474bbf0
tianjiangyu <161840237@nuaa.edu.cn>
2022-08-22 15:13:46 +0800

    stage3 Vue整体布局完善

--- README.md
+++ README.md
@@ -17,10 +17,29 @@ npm run serve
 
 
 
+
+
 #### 使用ElementUI快速搭建
 
 + assets目录下配置global.css全局样式
 + main.js中引入global.css全局样式
 + 设置全局stype height=100%
 + 逐层构建整体布局（熟悉模块规划）
+
+
+
+
+
+#### Vue整体布局完善
+
++ 调整目录高度为100%  el-container	 style="height: 100vh"
++ 更换menu颜色： el-menu    background-color="rgb(48,65,86)"
++ 增加menu字体颜色和高亮设置
++ 完善菜单栏的收缩功能和展开功能(根据ElementUI官网进行修改)
+
++ 增加菜单栏标题以及图表 - 后台管理系统，并绑定vue v-show
+
++ 增加边栏的阴影效果
+
+
 
--- springboot-project/vue/src/views/HomeView.vue
+++ springboot-project/vue/src/views/HomeView.vue
@@ -1,11 +1,31 @@
 <template>
   <div style="height: 100%">
-    <el-container style="height: 100%">
+    <el-container style="height: 100vh">
 
-      <el-aside width="200px" style="background-color: rgb(238, 241, 246);height: 100%">
-        <el-menu :default-openeds="['1', '3']" style="height: 100%">
+      <el-aside width="sideWidth + 'px'"
+                style="
+                background-color: rgb(238, 241, 246);
+                height: 100%;
+                box-shadow: 2px 0 6px rgb(0 21 41/35%);
+                "
+      >
+        <el-menu :default-openeds="['1', '3']" style="min-height: 100%"
+                  background-color="rgb(48,65,86)"
+                 text-color="#fff"
+                 overflow-x:hidden
+                 active-text-color="#ffd04b"
+                 :collapse-transition="false"
+                 :collapse="isCollapse"
+        >
+          <div style="height: 60px;line-height: 60px;text-align: center">
+            <img src="../assets/logo.png" alt="" style="width: 20px;position: relative;top: 5px;margin-right: 5px" >
+            <b style="color: aliceblue" v-show="logoTextShow">后台管理系统</b>
+          </div>
           <el-submenu index="1">
-            <template slot="title"><i class="el-icon-message"></i>导航一</template>
+            <template slot="title">
+              <i class="el-icon-message"></i>
+              <span>导航一</span>
+            </template>
             <el-menu-item-group>
               <template slot="title">分组一</template>
               <el-menu-item index="1-1">选项1</el-menu-item>
@@ -20,7 +40,10 @@
             </el-submenu>
           </el-submenu>
           <el-submenu index="2">
-            <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>导航二</span>
+            </template>
             <el-menu-item-group>
               <template slot="title">分组一</template>
               <el-menu-item index="2-1">选项1</el-menu-item>
@@ -35,7 +58,10 @@
             </el-submenu>
           </el-submenu>
           <el-submenu index="3">
-            <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+            <template slot="title">
+              <i class="el-icon-setting"></i>
+              <span>导航三</span>
+            </template>
             <el-menu-item-group>
               <template slot="title">分组一</template>
               <el-menu-item index="3-1">选项1</el-menu-item>
@@ -54,16 +80,18 @@
 
       <el-container>
 
-        <el-header style="text-align: right; font-size: 12px ; border: 1px solid;line-height: 60px">
-          <el-dropdown>
-            <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-header style="font-size: 12px ; border: 1px solid;line-height: 60px ; display:flex">
+          <div style="flex:1;font-size: 18px">
+            <span :class="collapseBtnClass" style="cursor: pointer" @click="collapse"></span>
+          </div>
+
+          <el-dropdown style="width: 100px; cursor: pointer">
+            <span>王小虎</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>查看</el-dropdown-item>
-              <el-dropdown-item>新增</el-dropdown-item>
-              <el-dropdown-item>删除</el-dropdown-item>
+              <el-dropdown-item>个人信息</el-dropdown-item>
+              <el-dropdown-item>退出</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
-          <span>王小虎</span>
         </el-header>
 
         <el-main>
@@ -97,9 +125,23 @@ export default {
       };
 
       return {
-          tableData: Array(10).fill(item),
-          msg: "hello tjyy"
+        tableData: Array(10).fill(item),
+        msg: "hello tjyy",
+        collapseBtnClass:'el-icon-s-fold',
+        isCollapse:false,
+        sideWidth: 200,
+        logoTextShow:true
       }
+  },
+  methods:{
+    collapse(){
+      this.isCollapse = !this.isCollapse
+      if(this.isCollapse) {
+        this.logoTextShow = false
+      }else{
+        this.logoTextShow = true
+      }
+    }
   }
 }
 </script>
